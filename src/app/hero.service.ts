531f04ffdef08ec78e06bc1efb1d5fb80d5aff25
sohumtiwary <sohumtiwary@gmail.com>
2023-06-19 00:38:46 -0700

    navigation hub

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -17,7 +17,9 @@ export class HeroService {
     return heroes;
   }
 
-  
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
-
-
